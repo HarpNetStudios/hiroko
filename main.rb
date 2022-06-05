ea8d1598fc31782796abe5e7c5039e45a4dcd876
Zsolt Zitting <yellowberry@cocaine.ninja>
2022-06-05 01:32:46 -0600

    this took me several months to find

--- main.rb
+++ main.rb
@@ -18,8 +18,8 @@ require 'discordrb'
 
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN'], intents: [Discordrb::ALL_INTENTS], name: "Hiroko"
 
-Main_server_id = ENV['DISCORD_SERVER_ID']
-Owner_id = ENV['DISCORD_OWNER_ID']
+Main_server_id = ENV['DISCORD_SERVER_ID'].to_i
+Owner_id = ENV['DISCORD_OWNER_ID'].to_i
 
 New_member_role_id = 961023612826054656
 
